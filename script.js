5f4ebd9ad98f609f9fbead9464a651f92555f9f8
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2021-09-23 12:34:55 +0530

    Completed Intro details

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ tl.to('.slider',{y:"-100%",duration:1.5,delay: 0.5});
 tl.to(".start",{y:"-100%",duration:1},"-=1");
 tl.fromTo('header', {opacity: 0},{opacity:1,duration:1});
 tl.fromTo('.itext1', {opacity: 0},{opacity:1,duration:1}, '-=1');
+tl.fromTo('#itext2', {opacity: 0},{opacity:1,duration:1}, '-=1');
+
